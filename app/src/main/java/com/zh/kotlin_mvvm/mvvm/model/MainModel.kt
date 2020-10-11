244fe6d622aedb095241539d8076ed433f4b9070
qq363 <363061015@qq.com>
2020-10-11 16:56:35 +0800

    更新是使用方法

--- app/src/main/java/com/zh/kotlin_mvvm/mvvm/model/MainModel.kt
+++ app/src/main/java/com/zh/kotlin_mvvm/mvvm/model/MainModel.kt
@@ -3,20 +3,12 @@ package com.zh.kotlin_mvvm.mvvm.model
 import com.zh.common.base.BaseMapToBody
 import com.zh.common.base.BaseObserver
 import com.zh.common.base.model.BaseModel
-import com.zh.common.exception.ResponseTransformer
-import com.zh.common.schedulers.SchedulerProvider
 import com.zh.kotlin_mvvm.net.INetService
 import com.zh.kotlin_mvvm.net.bean.LoginBean
 
 class MainModel : BaseModel<INetService>(INetService::class.java) {
 
     fun onLogin(map: Map<String, Any>, observer: BaseObserver<LoginBean>) {
-        doNetRequest(getINetService().login(BaseMapToBody.convertMapToBody(map)) ,observer)
-
-
-        /*getINetService().login(BaseMapToBody.convertMapToBody(map))
-            .compose(ResponseTransformer.handleResult())
-            .compose(SchedulerProvider.instance.applySchedulers())
-            .subscribe(observer)*/
+        doNetRequest(getINetService().login(BaseMapToBody.convertMapToBody(map)), observer)
     }
 }
