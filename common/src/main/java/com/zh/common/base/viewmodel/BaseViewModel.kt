cc8d811474fe9606a84505839c5e3e0b28db46a9
qq363 <363061015@qq.com>
2020-10-11 17:01:18 +0800

    更新是使用方法

--- common/src/main/java/com/zh/common/base/viewmodel/BaseViewModel.kt
+++ common/src/main/java/com/zh/common/base/viewmodel/BaseViewModel.kt
@@ -1,29 +1,21 @@
 package com.zh.common.base.viewmodel
 
-import androidx.core.util.Consumer
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import com.zh.common.base.model.BaseModel
-import io.reactivex.disposables.Disposable
 
 /**
  * @auth xiaohua
  * @time 2020/10/8 - 10:02
  * @desc ViewModel基类
  */
-open class BaseViewModel<MODEL : BaseModel<*>?>(model: MODEL) : ViewModel(), IBaseViewModel,
-    Consumer<Disposable> {
+open class BaseViewModel<MODEL : BaseModel<*>?>(model: MODEL) : ViewModel(), IBaseViewModel {
 
     var mModel: MODEL? = null
 
     init {
         mModel = model
-    }
-
-    //绑定网络请求
-    override fun accept(disposable: Disposable) {
-        mModel?.addSubscribe(disposable)
     }
 
     override fun onAny(owner: LifecycleOwner?, event: Lifecycle.Event?) {
