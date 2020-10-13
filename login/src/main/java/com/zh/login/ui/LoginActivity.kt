4811be10ead90fb7b7891fcf6b6e14a6ca43cb32
qq363 <363061015@qq.com>
2020-10-13 23:31:17 +0800

    修改抖动的bug

--- login/src/main/java/com/zh/login/ui/LoginActivity.kt
+++ login/src/main/java/com/zh/login/ui/LoginActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.zh.common.base.BaseActivity
 import com.zh.common.base.viewmodel.NormalViewModel
+import com.zh.common.utils.LogUtil
 import com.zh.config.ZjConfig
 import com.zh.login.R
 import kotlinx.android.synthetic.main.activity_login.*
@@ -32,6 +33,7 @@ class LoginActivity : BaseActivity<ViewDataBinding, NormalViewModel>() {
         var afterY = 0
         appBarLayout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
             override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                LogUtil.e("verticalOffset  $verticalOffset")
                 //当高度改变就重新设置子布局的高度
                 if (afterY != abs(verticalOffset)) {
                     var lps: CollapsingToolbarLayout.LayoutParams =
