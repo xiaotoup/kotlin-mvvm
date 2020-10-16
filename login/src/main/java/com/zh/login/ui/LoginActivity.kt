f1de7fb5f15c0c18eb5c995efb5fa6cee804b519
hpb <1258462576@qq.com>
2020-10-16 14:21:09 +0800

    BUG

--- login/src/main/java/com/zh/login/ui/LoginActivity.kt
+++ login/src/main/java/com/zh/login/ui/LoginActivity.kt
@@ -36,18 +36,15 @@ class LoginActivity : BaseActivity<ViewDataBinding, NormalViewModel>() {
                 LogUtil.e("verticalOffset  $verticalOffset")
                 LogUtil.e("toolbarLayout  ${toolbarLayout.measuredHeight}")
                 //当高度改变就重新设置子布局的高度
-                if (afterY != abs(verticalOffset)) {
-                    /*var lps: CollapsingToolbarLayout.LayoutParams =
-                        changeView.layoutParams as CollapsingToolbarLayout.LayoutParams
-                    //得到滑动后可视子布局的高度
-                    lps.height = appBarLayout.measuredHeight - abs(verticalOffset)
-                    changeView.layoutParams = lps*/
-                    val scale =
-                        abs(changeView.measuredHeight + verticalOffset).toFloat() / changeView.measuredHeight
-                    changeView.scaleX = scale
-                    changeView.scaleY = scale
-                }
-                afterY = abs(verticalOffset)
+                /*var lps: CollapsingToolbarLayout.LayoutParams =
+                    changeView.layoutParams as CollapsingToolbarLayout.LayoutParams
+                //得到滑动后可视子布局的高度
+                lps.height = appBarLayout.measuredHeight - abs(verticalOffset)
+                changeView.layoutParams = lps*/
+                val scale =
+                    (changeView.measuredHeight + verticalOffset).toFloat() / changeView.measuredHeight
+                changeView.scaleX = scale
+                changeView.scaleY = scale
             }
         })
     }
