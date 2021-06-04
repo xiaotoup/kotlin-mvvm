afdf6733fc408b5dfdb355a316deb986c8f7de0a
zhenghua <363061015@qq.com>
2021-06-04 16:24:20 +0800

    ss

--- app/src/main/java/com/zh/kotlin_mvvm/ui/PictureActivity.kt
+++ app/src/main/java/com/zh/kotlin_mvvm/ui/PictureActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.databinding.ViewDataBinding
 import com.zh.common.base.BaseActivity
 import com.zh.common.base.viewmodel.BaseViewModel
+import com.zh.common.base.viewmodel.NormalViewModel
 import com.zh.config.fragment.PictureFragment
 import com.zh.kotlin_mvvm.R
 import kotlinx.android.synthetic.main.activity_picture.*
 
 class PictureActivity(
     override val layoutRes: Int = R.layout.activity_picture,
-    override val viewModel: BaseViewModel = BaseViewModel()
+    override val viewModel: BaseViewModel = NormalViewModel()
 ) : BaseActivity<ViewDataBinding>() {
 
     private lateinit var pictureFragment: PictureFragment
